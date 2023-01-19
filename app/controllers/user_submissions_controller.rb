e99c1c2ba42636a97bac1cff5d10fa40adb26471
kiwanuka94 <samkimball4@gmail.com>
2023-01-18 21:57:45 -0500

    migration to store intended subscription plan for UserSubmission and added UserSubmission validations

--- app/controllers/user_submissions_controller.rb
+++ app/controllers/user_submissions_controller.rb
@@ -1,11 +1,10 @@
 class UserSubmissionsController < ApplicationController
   def create
-    puts "WHITELISTED PARAMS: #{user_submission_params}"
     UserSubmission.create!(user_submission_params)
-    redirect_to root_path
+    redirect_to root_path, notice: 'Thanks. Your application is being reviewed.'
   end
 
   def user_submission_params
-    params.require(:user_submission).permit(:first_name, :last_name, :email, :website, :text, :job_role)
+    params.require(:user_submission).permit(:first_name, :last_name, :email, :website, :text, :job_role, :plan_name)
   end
 end
